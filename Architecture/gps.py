43748cd1e2f4aebb903094af6bf00d30f9f1fe86
dhruvparamhans <dhruvparamhans@gmail.com>
2014-10-13 03:29:53 +0200

    Added Sensor.py
    Sensor.py contains functionalities common to all sensors

--- Architecture/gps.py
+++ Architecture/gps.py
@@ -2,7 +2,10 @@ class GPS:
     def __init__(self):
         self.data_position = [] ##Position data
         self.data_velocity = [] ##Velocity data
-        self.data_latitude = 0
-        self.data_longitude = 0
-        self.data_altitude = 0
+        self.data_latitude = 0 ##Latitude
+        self.data_longitude = 0 ##Longitude
+        self.data_altitude = 0 ##Altitude
+
+
+
 
---
+++ Architecture/sensor.py
@@ -0,0 +1,12 @@
+##Contains functionalities common to all sensor classes
+
+class sensor:
+    def __init__(self):
+        self.device_code = 0 ##Specific Device code for all sensor
+    def power_on_sensor(self):
+        ##Insert Code for powering up sensor
+        return 0
+    def power_off_sensor(self):
+        ##Insert code for powering off sensor
+        return 0
+
