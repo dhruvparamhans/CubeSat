981502592a8e9272f691d869f73ffea967e19764
dhruvparamhans <dhruvparamhans@gmail.com>
2014-10-13 15:15:07 +0200

    Added rtc.py
    Class for Real Time Clock

---
+++ Architecture/rtc.py
@@ -0,0 +1,9 @@
+class rtc:
+    def __init__(self):
+        self.date = [];##Date list DD/MM/YYYY
+        self.time = [];##Time list HH:MM:SS
+    def get_time(self):
+        ##Insert code to get time from ODB
+        return(self.date, self.time)
+
+
